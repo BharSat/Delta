90bdf7895d0681c527c3641df98bd736d9c94e0b
bhargav <bhargavmbalaji@gmail.com>
2022-06-25 22:24:07 +0530

    v1.1.1

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ rules_page = function () {
     window.location.href = getParentFolder() + "/rules.html"
 }
 
+resize = function () {
+    let height = 700;
+    let width = 1400;
+    document.getElementById("details").style.width = Math.round(document.getElementById("details").getBoundingClientRect().width*(1.5-(window.innerWidth/width))) + "px"
+    document.getElementById("details").style.height = Math.round(document.getElementById("details").getBoundingClientRect().height*(window.innerHeight/height)) + "px"
+    console.log((1.5+(window.innerWidth/width)) + "px")
+}
